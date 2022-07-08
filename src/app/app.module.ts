6da3c1d651416088ebdb4f62882a407ea8a40508
viji <vijayalakshmi.r@agiratech.com>
2022-07-08 18:39:46 +0530

    Added Customer-app lazy loading sample project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersRoutingModule } from './customers/customers-routing.module';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomersModule } from './customers/customers.module';
+import { OrdersRoutingModule } from './orders/orders-routing.module';
+import { OrdersComponent } from './orders/orders.component';
+import { OrdersModule } from './orders/orders.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustomersRoutingModule,
+    OrdersRoutingModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
